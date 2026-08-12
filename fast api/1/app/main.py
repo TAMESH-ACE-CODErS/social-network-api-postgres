071c1f7b69f16810c0369a7eab5613c25da77a43
TAMESH-ACE-CODErS <tamesh.success.1@gmail.com>
2026-08-12 10:24:16 +0530

    more functions on users and hashing

--- fast api/1/app/main.py
+++ fast api/1/app/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI, Response, status, HTTPException, Depends
 from typing import List
 import psycopg2
 from psycopg2.extras import RealDictCursor
-
+from passlib.context import CryptContext
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas,utils
 from .database import engine, SessionLocal
 
 # Create all tables in the database (if they don't exist)
 models.Base.metadata.create_all(bind=engine)
+
+pwd_context=CryptContext(schemes=['bcrypt'],deprecated="auto") #use of hashing 
 
 app = FastAPI()
 
@@ -95,10 +97,26 @@ def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends
     db.commit()
     return post_query.first()
 
+#this for user creations
 @app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    
+    # hash the passwords - user . password 
+    hashed_password=utils.hash(user.password)
+    user.password=hashed_password
+    
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+    
     return new_user
+
+@app.get('/users/{id}')
+def get_user(id:int, db:Session=Depends(get_db),response_model=schemas.UserOut):
+    user=db.query(models.User).filter(models.User.id==id).first() 
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id: {id}")
+    
+    return user
